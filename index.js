0fd67a52b02468670128222f996f4a615d6fef84
Aayush Poudel <aayushp2906@gmail.com>
2024-06-07 12:20:31 +0545

    declaring the port for the nodejs application

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require("express");
 
 //creating an instance of the Express application.
 const app = express();
+
+//declaring a constant variable named 'PORT' & assigning it the value '5000', the port number on which the
+//server will listen for upcoming connections.
+const PORT = 5000;
