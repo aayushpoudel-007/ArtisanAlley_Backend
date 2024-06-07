9b83624304af908d217e925265a94b004e34bde0
Aayush Poudel <aayushp2906@gmail.com>
2024-06-07 12:29:07 +0545

    Making the app listen to the port 5000 and display a message when it has successfully done so

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const app = express();
 //declaring a constant variable named 'PORT' & assigning it the value '5000', the port number on which the
 //server will listen for upcoming connections.
 const PORT = 5000;
+
+//listening on the specified port(5000), and once it starts 
+//logs a message to the console indicating that the server is up and running on that port.
+app.listen(PORT, () => {
+    console.log(`The server is up & running on port ${PORT}`);
+});
