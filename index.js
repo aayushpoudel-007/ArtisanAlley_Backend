390c9cca124ba3b8276e1a77d592530677068a91
Aayush Poudel <aayushp2906@gmail.com>
2024-06-13 08:02:03 +0545

    changed the variable PORT to port

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express();
 
 //declaring a constant variable named 'PORT' & assigning it the value '5000', the port number on which the
 //server will listen for upcoming connections.
-const PORT = 5000;
+const port = 5000;
 
 //listening on the specified port(5000), and once it starts 
 //logs a message to the console indicating that the server is up and running on that port.
-app.listen(PORT, () => {
-    console.log(`The server is up & running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`The server is up & running on port ${port}`);
 });
 
 //creating a test route
