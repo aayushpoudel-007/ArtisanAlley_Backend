1f1a8a73479ebf5c639a9432d98b41168417a2b2
Aayush Poudel <aayushp2906@gmail.com>
2024-06-13 08:22:38 +0545

    updated db.js to show errors and connect to the database

--- database/db.js
+++ database/db.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+console.log('dotenv loaded successfully');
+console.log('db_url:', process.env.db_url);
+
 const mongoose = require("mongoose");
 
 const connectDB = () => {
     mongoose.connect(process.env.db_url).then(() => {
         console.log("Connected to the database");
-    })
+    }).catch(err => {
+        console.error("Error connecting to the database:", err);
+    });
 }
 
 module.exports = connectDB;
